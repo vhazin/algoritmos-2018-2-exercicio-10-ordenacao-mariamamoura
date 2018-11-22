2c94e07a08bf46026b64977e45f468a6afbfcf7c
[Mariama Moura] <[mouramariama@gmail.com]>
2018-11-22 00:15:39 -0200

    a descartar

--- ordenacao2.c
+++ ordenacao2.c
@@ -17,19 +17,20 @@ void trocar(int *num1, int *num2){
 //void imprimir(lista[], int size){
 
 //}
-
 int main(){
     int num_criancas;
     scanf("%d", &num_criancas);
-    char nome_criancas[num_criancas], comportamento[5];
-    char lista[num_criancas][2];
+    char nome_criancas[num_criancas], comportamento[num_criancas];
+    char lista[num_criancas][3];
 
     for (int i=0; i< num_criancas; i++){
         scanf("%s %s", &comportamento[i], &nome_criancas[i]);
         lista[i][0] = comportamento[i];
-        lista[i][1] = nome_criancas[i];
+        lista[i][2] = nome_criancas[i];
 
     } printf(lista);
 
     return 0;
 }
+
+
